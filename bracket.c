0d2810edc579f9a59be1a3c1a12738eca0ef3b64
weitsang <weitsang@yahoo.com>
2020-10-25 05:56:01 +0000

    First commit

---
+++ bracket.c
@@ -0,0 +1,13 @@
+/**
+ * CS1010 Semester 1 AY18/19
+ * Practical Exam 2: Bracket 
+ *
+ * @file: bracket.c
+ * @author: XXX (Group YYY)
+ */
+
+#include "cs1010.h"
+
+int main()
+{
+}
---
+++ transpose.c
@@ -0,0 +1,14 @@
+/**
+ * CS1010 Semester 1 AY18/19
+ * Practical Exam 2: Transpose 
+ *
+ * @file: transpose.c
+ * @author: XXX (Group YYY)
+ */
+
+#include "cs1010.h"
+#include <stdbool.h>
+
+int main()
+{
+}
